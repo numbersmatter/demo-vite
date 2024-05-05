9a204bdd4ed742e730838c1c23374afd79dfcceb
MarkMutt <lleasy8@gmail.com>
2024-05-05 15:42:03 -0400

    setting up shell

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,7 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { MetaFunction, LoaderFunctionArgs } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { Outlet, useLoaderData, } from "@remix-run/react";
+import { StaffShell } from "~/components/shell/staff-shell";
 
 export const meta: MetaFunction = () => {
   return [
@@ -7,35 +10,26 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+
+export const loader = async ({ request, params }: LoaderFunctionArgs) => {
+
+  return json({});
+};
+
+
+
+
+
 export default function Index() {
+  const data = useLoaderData<typeof loader>()
+
+
   return (
-    <div className="">
-      <h1 className="text-4xl">Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+    <StaffShell >
+      <div className="flex-1">
+
+        <Outlet />
+      </div>
+    </StaffShell>
   );
 }
